a81debfe3ea777eb6bec722be3645cd7eacd7557
Ahmedlaghari <113627477+Ahmedlaghari@users.noreply.github.com>
2025-12-02 13:32:58 +0500

    Fix loop condition and calculate length of x

--- src/derivativeb.cpp
+++ src/derivativeb.cpp
@@ -3,8 +3,8 @@ float derivativeb(float* x, float* y, float w, float b) {
     int c = 0;
     float total = 0.0f;
     float rtotal = 0.0f;
-
-    while (c != 46) {
+    int len = x,size();
+    for (int i = 0 ; i < len ; i++ {
         float err = -((y[c]) - (w * x[c] + b));
         c++;
         total = total + err;

cce39ceb96cdb9284cb3afbf624689227853efed
Ahmedlaghari <113627477+Ahmedlaghari@users.noreply.github.com>
2025-12-02 13:28:34 +0500

    Generate x and y arrays for sine calculation
    Added code to generate and display x and y arrays for sine function.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,21 @@
 #include"derivativew.h"
 #include"linear.h"
 int main() {
-	linear();
+	const int N = 50;
+    float start = 0.0;
+    float end   = 2 * M_PI;
+    float step  = (end - start) / (N - 1);
+
+    cout << "float x[] = { ";
+    for (int i = 0; i < N; i++) {
+        cout << start + i * step;
+        if (i != N - 1) cout << ", ";
+    }
+    cout << "float y[] = { ";
+    for (int i = 0; i < N; i++) {
+        cout << sin(start + i * step);
+        if (i != N - 1) cout << ", ";
+    }
+	linear(x,y);
 	return 0;
 }
